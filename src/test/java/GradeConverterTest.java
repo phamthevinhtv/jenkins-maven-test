02bbc8f12b28f3fb83ad4c874bf2cf90e97b7d9a
phamthevinhtv <phamthevinhtv@gmail.com>
2025-06-12 08:52:07 +0700

    Them test moi

--- src/test/java/GradeConverterTest.java
+++ src/test/java/GradeConverterTest.java
@@ -3,7 +3,6 @@ import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class GradeConverterTest {
 
     private GradeConverter  converter;
@@ -19,7 +18,6 @@ public class GradeConverterTest {
         String actual = converter.convert(100);
         assertEquals(expected,actual);
     }
-
     @Test
     public void testConvert2() throws Exception {
         String expected = "A";
@@ -29,39 +27,67 @@ public class GradeConverterTest {
     @Test
     public void testConvert3() throws Exception {
         String expected = "B";
-        String actual = converter.convert(80);
+        String actual = converter.convert(89);
         assertEquals(expected,actual);
     }
     @Test
     public void testConvert4() throws Exception {
+        String expected = "B";
+        String actual = converter.convert(80);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testConvert5() throws Exception {
+        String expected = "C";
+        String actual = converter.convert(79);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testConvert6() throws Exception {
         String expected = "C";
         String actual = converter.convert(70);
         assertEquals(expected, actual);
     }
     @Test
-    public void testConvert5() throws Exception {
+    public void testConvert7() throws Exception {
+        String expected = "D";
+        String actual = converter.convert(69);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testConvert8() throws Exception {
         String expected = "D";
         String actual = converter.convert(60);
         assertEquals(expected,actual);
     }
     @Test
-    public void testConvert6() throws Exception {
+    public void testConvert9() throws Exception {
+        String expected = "E";
+        String actual = converter.convert(59);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testConvert10() throws Exception {
         String expected = "E";
         String actual = converter.convert(50);
         assertEquals(expected,actual);
     }
     @Test
-    public void testConvert7() throws Exception {
+    public void testConvert11() throws Exception {
+        String expected = "E";
+        String actual = converter.convert(0);
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testConvert12() throws Exception {
         String expected = "Invalid";
         String actual = converter.convert(101);
         assertEquals(expected,actual);
     }
     @Test
-    public void testConvert8() throws Exception {
+    public void testConvert13() throws Exception {
         String expected = "Invalid";
         String actual = converter.convert(-10);
         assertEquals(expected,actual);
     }
-
-
 }
